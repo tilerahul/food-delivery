c47a596498945e63b1e2f1b6d4a977d6c18b7322
Rahul Tile <tilerahul8068@gmail.com>
2024-04-04 09:21:43 +0530

    final project

---
+++ FrontEnd1/src/components/layout/Footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <>
+      <footer>
+      <hr/>
+        <p className="text-center">
+          <b>Thank You for Visit this Website.</b>
+        <p>Food Delivery Website - 2023-2024, All Rights Reserved...</p>
+        </p>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
